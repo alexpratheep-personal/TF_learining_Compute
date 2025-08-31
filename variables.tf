6f3ad3fdc4bd1a724bfef0aab6aa90a521f2cdbf
alexpratheep <alexpratheep@live.com>
2025-08-31 20:21:22 +0530

    vm-deploy

--- variables.tf
+++ variables.tf
@@ -5,3 +5,14 @@ variable "location" {
   default = "UK South"
 }
 
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureuser" # optional; you can override in TFC
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM (Windows VM only)"
+  type        = string
+  sensitive   = true
+}
